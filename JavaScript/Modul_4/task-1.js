502aace5fc06eee00637a5bd0cd7abd44d2a775f
lenya1976 <bargamon76@gmail.comgit config --global user.name Your>
2020-02-16 21:27:59 +0200

    Modul_4

--- JavaScript/Modul_4/task-1.js
+++ JavaScript/Modul_4/task-1.js
@@ -27,53 +27,28 @@
 // console.log(account.balance); // 19000
 // console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
 
-// const account = {
-//   owner: 'Mango',
-//   balance: 24000,
-//   discount: 0.1,
-//   orders: ['order-1', 'order-2', 'order-3'],
-//   changeDiscount(value) {
-//     this.discount = value;
-//   },
-//   showOrders() {
-//     return this.orders;
-//   },
-//   addOrder(cost, order) {
-//     this.balance -= cost;
-//     this.orders.push(order);
-//   },
-// };
-
-// account.changeDiscount(0.15);
-// console.log(account.discount); // 0.15
-
-// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
-
-// account.addOrder(5000, 'order-4');
-// console.log(account.balance); // 19000
-// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
-
-// const createTransaction = function(amount, type) {
-//   return {
-//     type: type,
-//     amount: amount,
-//   };
-// };
-
-// console.log(createTransaction(500, 'dsx'));
-
-const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
 };
 
-deposit(amount => {
-  let type = Transaction.DEPOSIT;
-  createTransaction(amount, type);
-  return {
-    type: type,
-    amount: amount,
-  };
-});
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-console.log(deposit(500));
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
